683d74e2856fe8f9a00787cd2482e9df8adf74e1
Awnder <amshiroma@zoho.com>
2024-04-17 21:46:10 -0700

    Overhauled get_water_data and added commandline args

--- streamflow_forecast.py
+++ streamflow_forecast.py
@@ -2,33 +2,79 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import hydrofunctions as hf
-import datetime
+from datetime import datetime
+from datetime import timedelta
 from calendar import isleap
+import argparse
 
+def get_commandline_input():
+    ''' Guides commandline parsing for streamflow program. Optional input string in form 'yyyy-mm-dd'. 
+        If no input is given, returns today's date.
+    '''
+    parser = argparse.ArgumentParser(description='Controls date input for streamflow graph.')
+    parser.add_argument('date_string', nargs='?', type=str, default='')
+    args = parser.parse_args()
 
-def get_historical_stream(start_datetime, end_datetime):
+    if args.date_string == '':
+        return datetime.now().date()
+    
+    try:
+        date = str_to_date(args.date_string)
+        return date
+    except Exception as e:
+        print(e)
+        return
+
+def get_water_data(anchor_date):
+    ''' Gets the past two weeks of water data from given date, plus the past nine years of water data two weeks before 
+        and one week after given date.
+    
+    Args:
+        anchor_date (date):
+            should take on form 'yyyy-mm-dd'
+    '''
+
     sensor = '11527000'
+    sd = anchor_date - timedelta(days=14)
+    ed = anchor_date + timedelta(days=6) # 6 + start_time
+    df = pd.DataFrame()
 
-    #one year ago, two years ago, etc
-    df_year1, df_year2, df_year3, df_year4, df_year5, df_year6, df_year7, df_year8, df_year9 = pd.DataFrame()
+    # retrieving water data for two weeks for this year (the third week is in the future)
+    # if date or time looks a bit odd, remember this is in UTC
+    data = hf.NWIS(sensor, 'iv', start_date=sd, end_date=anchor_date)
+    df = data.df('discharge')
 
-    for year in range(9):
-        delta = datetime.timedelta(days=365)
-        if isleap(start_datetime.year - delta):
-            delta = datetime.timedelta(days=366)
-            data = hf.NWIS(sensor, 'iv', start_date=start_datetime-delta, end_date=end_datetime-delta)
+    # retrieving water data for three weeks for the past 9 years
+    for i in range(9):
+        delta = timedelta(days=365)
+        if isleap((anchor_date - delta).year): # taking into account leap years
+            delta = timedelta(days=366)
+            sd -= delta
+            ed -= delta
+            data = hf.NWIS(sensor, 'iv', start_date=sd, end_date=ed)
         else:
-            data = hf.NWIS(sensor, 'iv', start_date=start_datetime-delta, end_date=end_datetime-delta)
-    water_dataframe = data.df('discharge')
+            sd -= delta
+            ed -= delta
+            data = hf.NWIS(sensor, 'iv', start_date=sd, end_date=ed)
+        df = pd.concat([df, data.df('discharge')]) # 'appending' to dataframe
+    
+    return df
 
-def str_to_date(string):
-    return datetime.datetime.strptime(string, "%Y%m%d")
+def str_to_date(date_string):
+    ''' Converts a string to a date
+    
+    Args:
+        date_string (str):
+            should be in the form 'yyyy-mm-dd'
+    '''
+    return datetime.strptime(date_string, "%Y-%m-%d").date()
 
 def plot_stream():
     pass
 
 def main():
-    pass
+    date = get_commandline_input()
+    df = get_water_data(date)
 
 if __name__ == "__main__":
     main()
