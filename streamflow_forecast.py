a0ac2a11943170ea88683e8062d9290102853dfa
Awnder <amshiroma@zoho.com>
2024-04-18 09:21:36 -0700

    Added derivative and integral methods

--- streamflow_forecast.py
+++ streamflow_forecast.py
@@ -25,7 +25,7 @@ def get_commandline_input():
         print(e)
         return
 
-def get_water_data(anchor_date):
+def get_streamflow_data(anchor_date):
     ''' Gets the past two weeks of water data from given date, plus the past nine years of water data two weeks before 
         and one week after given date.
     
@@ -38,11 +38,13 @@ def get_water_data(anchor_date):
     sd = anchor_date - timedelta(days=14)
     ed = anchor_date + timedelta(days=6) # 6 + start_time
     df = pd.DataFrame()
+    df_list = []
 
     # retrieving water data for two weeks for this year (the third week is in the future)
     # if date or time looks a bit odd, remember this is in UTC
     data = hf.NWIS(sensor, 'iv', start_date=sd, end_date=anchor_date)
     df = data.df('discharge')
+    df_list.append(df)
 
     # retrieving water data for three weeks for the past 9 years
     for i in range(9):
@@ -56,9 +58,58 @@ def get_water_data(anchor_date):
             sd -= delta
             ed -= delta
             data = hf.NWIS(sensor, 'iv', start_date=sd, end_date=ed)
-        df = pd.concat([df, data.df('discharge')]) # 'appending' to dataframe
+        # df = pd.concat([df, data.df('discharge')]) # 'appending' to dataframe for a single dataframe with all years' data
+        df = data.df('discharge')
+        df_list.append(df)
     
-    return df
+    return df_list
+
+def get_streamflow_change(df):
+    ''' Gets the rate of instantaneous change in CFS/hr at the last two points of the dataframe
+
+    Args:
+        df (pandas dataframe)
+    '''
+
+    streamflow = df.iloc[:,0]
+    water0, water1 = streamflow.tail(2)    
+    deltaY = water0 - water1
+    deltaX = 15
+    return (deltaY / deltaX) * 60
+
+def get_streamflow_volume(df):
+    ''' Gets the total volume of an interval in cubic feet 
+    
+    Args:
+        df (pandas dataframe)
+    '''
+
+    streamflow = df.iloc[:,0]
+    
+    # calculate the middle riemann sum
+    width = 15 * 60 # min to sec as flow measured in CFS
+    avg_sum = 0
+    for point in range(len(streamflow)-1):
+        height = streamflow.iloc[point]
+        difference = (streamflow.iloc[point+1] - height) / 2 # find difference between two points and add it to first point
+        avg_sum += width * (height + difference)
+    return avg_sum
+
+def get_special_streamflow(df_list):
+    ''' Using the total volume of a given streamflow, gets the highest, lowest, average and returns their dataframes
+    
+    Args:
+        df_list (list of pandas dataframes)
+    '''
+
+    volume_list = []
+    for df in df_list:
+        volume_list.append(get_streamflow_volume(df))
+
+    df_max_inx = volume_list.index(max(volume_list))
+    df_min_idx = volume_list.index(min(volume_list))
+    df_avg_idx = volume_list.index(sum(volume_list)/len(volume_list))
+
 
 def str_to_date(date_string):
     ''' Converts a string to a date
@@ -67,6 +118,7 @@ def str_to_date(date_string):
         date_string (str):
             should be in the form 'yyyy-mm-dd'
     '''
+
     return datetime.strptime(date_string, "%Y-%m-%d").date()
 
 def plot_stream():
@@ -74,7 +126,11 @@ def plot_stream():
 
 def main():
     date = get_commandline_input()
-    df = get_water_data(date)
+    df_list = get_streamflow_data(date)
+    instant_rate = get_streamflow_change(df_list[0])
+
+
+    print(get_streamflow_volume(df_list[0]))
 
 if __name__ == "__main__":
     main()
