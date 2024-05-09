3d7f3c819befca5d9af2c401f18c4185b5bc8643
Andrew Shiroma <amshiroma@zoho.com>
2024-05-08 17:14:33 -0700

    Added a plot title

--- streamflow_forecast.py
+++ streamflow_forecast.py
@@ -264,6 +264,7 @@ def plot_streamflow():
     hist_last_index = len(df_hist_idx)-1
 
     plt.figure(figsize=(14,9))
+    plt.title('Streamflow Graph')
 
     plt.plot(df_hist_idx, df_max['streamflow'], label = f'Highest ({df_max.iat[2,1]})')
     plt.plot(df_hist_idx, df_min['streamflow'], label = f'Lowest ({df_min.iat[2,1]})')
